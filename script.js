700fb5ba0efaea54f8f35207b0b2f952847e263b
Franco Siccatto <fsiccatto@gmail.com>
2022-06-02 14:54:54 -0300

    Galeria terminada

--- script.js
+++ script.js
@@ -20,5 +20,33 @@
 
 /* ____________GALERIA____________ */
 ((d) => {
+  const $slides = d.querySelectorAll(".gallery-slide"),
+    $prevBtn = d.querySelector(".prev-btn"),
+    $nextBtn = d.querySelector(".next-btn");
+  let i = 0;
   
+  d.addEventListener("click", (e) => {
+    if (e.target === $prevBtn) {
+      e.preventDefault(); //Para que no se recargue la pagina
+      $slides[i].classList.remove("active");
+      i--;
+      
+      if (i < 0) { //Si llega a ser menor que 0, quiere decir que es el primer slide y se debe regresar al ultimo.
+        i = $slides.length - 1;
+      }
+      
+      $slides[i].classList.add("active");
+    }
+    if (e.target === $nextBtn) {
+      e.preventDefault();
+      $slides[i].classList.remove("active");
+      i++;
+      console.log("Next")
+      if (i > $slides.length - 1) {
+        i = 0;
+      }
+
+      $slides[i].classList.add("active");
+    }
+  });
 })(document);
