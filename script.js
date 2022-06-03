6b82018592f4c5d7211bd2404e1fdd5c2bcead89
Franco Siccatto <fsiccatto@gmail.com>
2022-06-03 11:32:16 -0300

    Boton de UP listo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const d = document,
+  w = window;
+
 /* ____________MENU____________ */
 ((d) => {
   const $btnMenu = d.querySelector(".menu-button"),
@@ -24,24 +27,25 @@
     $prevBtn = d.querySelector(".prev-btn"),
     $nextBtn = d.querySelector(".next-btn");
   let i = 0;
-  
+
   d.addEventListener("click", (e) => {
     if (e.target === $prevBtn) {
       e.preventDefault(); //Para que no se recargue la pagina
       $slides[i].classList.remove("active");
       i--;
-      
-      if (i < 0) { //Si llega a ser menor que 0, quiere decir que es el primer slide y se debe regresar al ultimo.
+
+      if (i < 0) {
+        //Si llega a ser menor que 0, quiere decir que es el primer slide y se debe regresar al ultimo.
         i = $slides.length - 1;
       }
-      
+
       $slides[i].classList.add("active");
     }
     if (e.target === $nextBtn) {
       e.preventDefault();
       $slides[i].classList.remove("active");
       i++;
-      console.log("Next")
+      console.log("Next");
       if (i > $slides.length - 1) {
         i = 0;
       }
@@ -51,7 +55,7 @@
   });
 })(document);
 
-/* __________FORMULARIO_______________ */
+/* ________________FORMULARIO__________________ */
 ((d) => {
   const $form = d.querySelector(".contact-form"),
     $inputs = d.querySelectorAll(".contact-form [required]");
@@ -93,5 +97,27 @@
       $form.reset();
       setTimeout(() => $response.classList.add("none"), 2000);
     }, 2000);
+  });
+})(document);
+
+/* ________________Boton UP__________________ */
+((d) => {
+  const $btnUp = d.querySelector(".up-btn");
+
+  d.addEventListener("scroll", () => {
+    if (d.documentElement.scrollTop > 300 || w.pageYOffset > 300) {
+      $btnUp.classList.remove("hidden");
+    } else {
+      $btnUp.classList.add("hidden");
+    }
+  });
+
+  $btnUp.addEventListener("click", (e) => {
+    if (e.target.matches(".up-btn")) {
+      w.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   });
 })(document);
